9186e8fe22dee5c1dfc2cd71e2e4efc53f96ebd6
Botond24 <44529287+Botond24@users.noreply.github.com>
2024-05-17 18:06:46 +0200

    ac evbgfnujoiíd

--- hamc-server-java/rootfs/etc/cont-init.d/run.sh
+++ hamc-server-java/rootfs/etc/cont-init.d/run.sh
@@ -5,9 +5,9 @@ bashio::log.info "Starting..."
 #rm -f /root/.config/ngrok/ngrok.yml
 mkdir /root/.config
 mkdir /root/.config/ngrok
-echo "tunnels:\n  first:\n    addr: 24454\n    proto: udp\n  second:\n    addr: 25565\n    proto: tcp" > /root/.config/ngrok/ngrok.yml
+echo "authtoken: $NGROK_API_KEY\ntunnels:\n  first:\n    addr: 24454\n    proto: udp\n  second:\n    addr: 25565\n    proto: tcp" > /root/.config/ngrok/ngrok.yml
 
-ngrok config add-authtoken $NGROK_API_KEY
+#ngrok config add-authtoken $NGROK_API_KEY
 ngrok start --all
 
 /start
