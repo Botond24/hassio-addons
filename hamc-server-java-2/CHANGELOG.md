207e95be6832a3ed89f2b07fafc247bdecdc6632
Botond24 <44529287+Botond24@users.noreply.github.com>
2024-07-28 19:51:55 +0200

    duplicate for second server

---
+++ hamc-server-java-2/CHANGELOG.md
@@ -0,0 +1,39 @@
+## 1.2.2
+
+- actually im not stupid
+
+## 1.2.1
+
+- reverting previous becasue im stupid
+
+## 1.2.0
+
+- change to java 17 for compat
+
+## 1.1.9
+
+- add normal fabric support
+
+## 1.1.7
+
+- remove ngrok cause im stoopid
+
+## 1.1.2
+
+- fix config
+
+## 1.0.9
+
+- add ngrok config
+
+## 1.0.8
+
+- add ngrok
+
+## 1.0.5
+
+- make my own
+
+## 1.0.0
+
+- Initial release
---
+++ hamc-server-java-2/rootfs/etc/cont-init.d/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bashio
+
+bashio::log.info "Starting..."
+
+#rm -f /root/.config/ngrok/ngrok.yml
+#mkdir /root/.config
+#mkdir /root/.config/ngrok
+#echo "authtoken: $NGROK_API_KEY\ntunnels:\n  first:\n    addr: 24454\n    proto: udp\n  second:\n    addr: 25565\n    proto: tcp" > /root/.config/ngrok/ngrok.yml
+
+#ngrok config add-authtoken $NGROK_API_KEY
+#ngrok start --all
+
+/start
